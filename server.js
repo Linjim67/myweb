063301de65268add33b1a8117dd3b072a98928ac
Linjim <linjunheng67@gmail.com>
2025-12-28 17:16:54 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files (CSS, Images) from the current folder
+app.use(express.static(__dirname));
+
+// Send the HTML file when the user goes to the home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Website is running at http://localhost:${port}`);
+});
